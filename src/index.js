a9ffa3254a88d9e822d1596b7d7e1ad4d3517cdf
Yauheni1988 <stigmat19@yandex.ru>
2020-03-22 11:47:46 +0300

    done reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let abs = Math.abs(n);
+    let numberToStr = abs.toString().split('');
+    numberToStr.reverse();
+    let res = numberToStr.join('');
+    return res;
+
+};
